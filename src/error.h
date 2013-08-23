a6db90e7a543149e49669c5a340e4004d465656a
dragonandlion <dragonandlion@126.com>
2013-08-23 11:18:22 +0800

    Create error.h
    错误码表

---
+++ src/error.h
@@ -0,0 +1,36 @@
+#ifndef D_ERROR_H
+#define D_ERROR_H
+
+
+#define CREATEFAIL    -1001
+#define OPENFAIL      -1002
+#define SHOWFAIL      -1003
+#define CLOSEFAIL     -1004
+#define SYSSERROR     -1111
+
+#ifdef D_ENGLISH
+
+#else
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
